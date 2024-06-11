7cf29c4adaa0b81545f6177f08af33e1097f18f7
Can Guven <cguven@wpi.edu>
2024-06-11 15:16:02 -0400

    options added

--- Register/Program.cs
+++ Register/Program.cs
@@ -18,23 +18,47 @@ namespace RegisterApplication{
 
     
         static void Main(string[] args) {
-            list_of_students.Add(new Student{Name = "Can", Email = "can@", Age = 22});
 
-            foreach (var student in list_of_students) {
-                Console.WriteLine($"Name: {student.Name}, Email: {student.Email}, Age: {student.Age}");
+            while(true){
+                Console.WriteLine("1. Add Student");
+                Console.WriteLine("2. Delete Student");
+                Console.WriteLine("3. Query Student");
+
+                var response = Console.ReadLine();
+                switch(response){
+
+                    case "1": 
+                        AddNewStudent();
+                        break;
+                    case "2":
+                        DeleteStudent();
+                        break;
+                    case "3":
+                        QueryStudent();
+                        break;  
+                    default:
+                        Console.WriteLine("choose one of the options");
+                        break;
+                }
             }
+                // list_of_students.Add(new Student{Name = "Can", Email = "can@", Age = 22});
+                // foreach (var student in list_of_students) {
+                //     Console.WriteLine($"Name: {student.Name}, Email: {student.Email}, Age: {student.Age}");
+                // }
 
         }
 
         static void AddNewStudent(){
-
+            Console.WriteLine("Add New Student");
         }
 
         static void DeleteStudent(){
+            Console.WriteLine("Delete Student");
 
         }
 
         static void QueryStudent(){
+            Console.WriteLine("Query Student");
 
         }
 
--- Register/obj/Debug/net8.0/Register.AssemblyInfo.cs
+++ Register/obj/Debug/net8.0/Register.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Register")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+723a8fd5c282f8aa98df6a20d1a187685ad66743")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3f9144e74f85285de3b23d2d8911bb9f7347ea1b")]
 [assembly: System.Reflection.AssemblyProductAttribute("Register")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Register")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
